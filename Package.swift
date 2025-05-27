676cf739a47cf4a51a51c007cf84a39e67f6ac09
Jimmy Li <jimmyli@m800.com>
2025-05-27 18:44:08 +0800

    split core

--- Package.swift
+++ Package.swift
@@ -9,26 +9,57 @@ let package = Package(
   ],
   products: [
     .library(
-      name: "FuseStorageKit",
-      targets: ["FuseStorageKit"]
+      name: "FuseStorageCore",
+      targets: ["FuseStorageCore"]
+    ),
+//    .library(
+//      name: "FuseStorageKit",
+//      targets: ["FuseStorage"]
+//    ),
+    .library(
+      name: "FuseStorageKitSQLCipher",
+      targets: ["FuseStorageSQLCipher"]
     ),
   ],
   dependencies: [
+    .package(url: "https://github.com/groue/GRDB.swift.git", from: "7.5.0"),
     .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "11.0.0"),
   ],
   targets: [
     .binaryTarget(
-      name: "GRDB",
+      name: "GRDBSQLCipher",
       url: "https://github.com/duckduckgo/GRDB.swift/releases/download/3.0.0/GRDB.xcframework.zip",
       checksum: "41f01022f6a35986393e063e1ef386fd896646ed032f7d0419c4b02fa3afe61d"
     ),
     .target(
-      name: "FuseStorageKit",
+      name: "FuseStorageCore",
       dependencies: [
-        "GRDB",
         .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
       ],
-      path: "Sources/FuseStorageKit",
+      path: "Sources/FuseStorageCore",
+      swiftSettings: [
+//        .define("USE_GRDB"),
+//        .define("SQLITE_HAS_CODEC"),
+      ]
+    ),
+//    .target(
+//      name: "FuseStorage",
+//      dependencies: [
+//        "GRDB",
+//        .product(name: "FirebaseStorage", package: "firebase-ios-sdk"),
+//      ],
+//      path: "Sources/FuseStorage",
+//      swiftSettings: [
+//        .define("USE_GRDB"),
+//      ]
+//    ),
+    .target(
+      name: "FuseStorageSQLCipher",
+      dependencies: [
+        "FuseStorageCore",
+        "GRDBSQLCipher",
+      ],
+      path: "Sources/FuseStorageSQLCipher",
       swiftSettings: [
         .define("USE_GRDB"),
         .define("SQLITE_HAS_CODEC"),
@@ -36,7 +67,7 @@ let package = Package(
     ),
     .testTarget(
       name: "FuseStorageKitTests",
-      dependencies: ["FuseStorageKit"],
+      dependencies: ["FuseStorageCore"],
       path: "Tests/FuseStorageKitTests"
     ),
   ]
---
+++ Sources/FuseStorage/dummy.swift
@@ -0,0 +1,7 @@
+//
+//  dummy.swift
+//  FuseStorageKit
+//
+//  Created by Jimmy on 2025/5/27.
+//
+
